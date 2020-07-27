399837603537027a46844ec10e8aa1f177fc7b7d
flingyp <1213563369@qq.com>
2020-07-27 16:11:38 +0800

    解决仓库文件夹打不开的问题

---
+++ blog/pages/_app.js
@@ -0,0 +1,5 @@
+import App from 'next/app'
+ 
+import 'antd/dist/antd.css' // Ant Design样式
+
+export default App
---
+++ blog/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Head from 'next/head'
+import {Button} from 'antd'
+
+const Home = () => (
+  <>
+    <Head>
+      <title>Home</title>
+    </Head>
+    <div>
+        <Button>我是按钮</Button>
+    </div>
+ </>
+)
+
+export default Home
